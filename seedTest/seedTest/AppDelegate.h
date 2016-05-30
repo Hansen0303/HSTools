9bc6ed86f30872bb4a6dc9d5c0a6c907047f5e96
hansen <hansen@hansen.local>
2016-05-30 15:33:22 +0800

    初始化项目

---
+++ seedTest/seedTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  seedTest
+//
+//  Created by hansen on 16/5/30.
+//  Copyright © 2016年 hansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ seedTest/seedTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  seedTest
+//
+//  Created by hansen on 16/5/30.
+//  Copyright © 2016年 hansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
